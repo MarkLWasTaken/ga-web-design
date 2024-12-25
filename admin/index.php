dea97ffc34ff8a95d2319e45ebb1cb5afeaec4cb
MarkLWasTaken <3318567+MarkLWasTaken@users.noreply.github.com>
2024-12-26 07:51:57 +0800

    Website updates and changes. 25 & 26 December 2024 Update: (Database SQL) New: admin/index.php file. New: css/admin.css file.
    Completed:
    Account login
    User session tracking
    Side navigation bar (with reference)
    HTML & CSS Quiz
    
    To-do:
    Layout for Account logout.
    
    Known issues:
    Navigation bar dropdown menu is not aligned correctly, and it doesn't fade out.
    Account registration not working (Partially fixed? To be tested).

---
+++ admin/index.php
@@ -0,0 +1,454 @@
+<?php
+session_start();
+
+// Include the PHP script for connecting to the database (DB).
+include '../php/connection.php';
+
+// Declare the variable to get the user ID and hide the warning message.
+@$userID = $_SESSION['id'];
+
+// Check if the guest or user logged in is an admin or not.
+if ($userID == null) {
+    // Do nothing.
+}
+else {
+    // Execute the query to get the user's role status.
+    $result = $connection->query("SELECT is_admin FROM users WHERE id = $userID");
+    while ($row = $result->fetch_assoc()) {
+        $isAdmin = (int) $row['is_admin']; // Cast to integer.
+    }
+}
+
+// Ensure the connection to the DB is closed, with or without any code execution for security reasons.
+// mysqli_close($connection);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Free web tutorials for everyone">
+    <meta name="keywords" content="HTML and CSS">
+    <meta name="author" content="CodingAssessment Group">
+
+    <title>CodingAssessment - About us</title>
+
+    <!-- Cascading Style Sheets -->
+    <link href="../css/styles.css" rel="stylesheet">
+    <link href="../css/dropdown-menu.css" rel="stylesheet">
+    <link href="../css/admin.css" rel="stylesheet">
+    <link href="../css/styles-rwd-mobile.css" rel="stylesheet">
+    <link href="../css/side-navigation-menu.css" rel="stylesheet">
+
+    <!-- JavaScripts -->
+    <script src="../js/side-navigation-menu.js"></script>
+</head>
+
+<body>
+    <!-- Referece: https://www.w3schools.com/howto/howto_js_sidenav.asp -->
+    <div id="side-navigation-menu" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" title="Close the side navigation menu.">&times;</a>
+        <a href="../index.php" onclick="closeNav()">Home</a>
+        <a href="../quizzes/index.php" onclick="closeNav()">Quizzes</a>
+        <a href="../tips/index.php" onclick="closeNav()">Tips</a>
+        <a href="../donations/index.php" onclick="closeNav()">Donations</a>
+        <a href="../contact/index.php" onclick="closeNav()">Contact us</a>
+        <a href="../../about/index.php" onclick="closeNav()">About us</a>
+        <?php
+        if (isset($_SESSION['email'])) {
+            echo "User is logged in.";
+            echo "<a href='../account/profile/index.php' onclick='closeNav()'>Profile</a>";
+            echo "<a href='../account/results/index.php' onclick='closeNav()'>Results</a>";
+            echo "<a href='../account/logout/index.php' onclick='closeNav()'>Logout</a>";
+        }
+        else {
+            echo "<a href='javascript:void(0)' style='opacity: 0;'>Blank space</a>";
+            echo "<a href='javascript:void(0)'>User is not logged in.</a>";
+            echo "<a href='../account/login/index.php' onclick='closeNav()'>Login</a>";
+            echo "<a href='../account/registration/index.php' onclick='closeNav()'>Register</a>";
+        }
+
+        if (isset($isAdmin) == 1) {
+            echo "<a href='#' onclick='closeNav()'>Admin</a>";
+        }
+        ?>
+    </div>
+    <!-- Referece: https://www.w3schools.com/howto/howto_js_sidenav.asp -->
+
+    <div id="basket">
+        <div id="circle-header"></div>
+
+        <div id="header" class="website-title">
+            <div id="header-2">
+                <br><br>
+                CodingAssessment
+            </div>
+        </div>
+
+        <div class="hidden-header-mobile"></div>
+
+        <br>
+
+        <div id="menu-buttons">
+            <div>
+                <a class="black-hyperlink" href="javascript:void(0)" onclick="openNav()">
+                    <div class="menu-button">
+                        <img src="../images/Hamburger_icon.svg" alt="Hamburger button icon for side navigation menu." title="Hamburger button icon for side navigation menu.">
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../index.php">
+                    <div class="menu-button">
+                        Home
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../quizzes/index.php">
+                    <div class="menu-button">
+                        Quizzes
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../tips/index.php">
+                    <div class="menu-button">
+                        Tips
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../donations/index.php">
+                    <div class="menu-button">
+                        Donations
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../contact/index.php">
+                    <div class="menu-button">
+                        Contact us
+                    </div>
+                </a>
+            </div>
+            <div>
+                <a class="black-hyperlink" href="../about/index.php">
+                    <div class="menu-button">
+                        About us
+                    </div>
+                </a>
+            </div>
+            <!-- TODO: Need help to fix the dropdown menu. -->
+            <div>
+                <!-- Prevent user from scrolling the page to the top when clicking on the "Username" button -->
+                <a class="black-hyperlink" href="javascript:void(0)">
+                    <div class="dropdown">
+                        <div class="menu-button">
+                            <?php
+                            if (isset($_SESSION['email'])) {
+                                // Online.
+                                echo "Account &#128994;";
+                            }
+                            else {
+                                // Offline.
+                                echo "Account &#128308;";
+                            }
+                            ?>
+                        </div>
+                        <!-- <br> -->
+                        <div class="dropdown-content">
+                            <?php
+                            if (isset($_SESSION['email'])) {
+                                echo "User is logged in.";
+                                echo "<a class='menu' href='../account/profile/index.php'>Profile</a>";
+                                echo "<a class='menu' href='../account/results/index.php'>Results</a>";
+                                echo "<a class='menu' href='../account/logout/index.php'>Logout</a>";
+                            }
+                            else {
+                                echo "User is not logged in.";
+                                echo "<a class='menu' href='../account/login/index.php'>Login</a>";
+                                echo "<a class='menu' href='../account/registration/index.php'>Register</a>";
+                            }
+                            ?>
+		                </div>
+                    </div>
+                </a>
+            </div>
+            <?php
+            if (isset($isAdmin) == 1) {
+                echo "<div>";
+                echo "<a class='black-hyperlink' href='#'>";
+                    echo "<div class='menu-button'>";
+                        echo "Admin";
+                    echo "</div>";
+                echo "</a>";
+            echo "</div>";
+            }
+            ?>
+        </div>
+
+        <br class="desktop-line-break">
+
+        <h1 class="page-title">Admin page (Manage table records)</h1>
+
+        <br>
+
+        <div class="query-table">
+            <form action="index.php" method="get">
+                <table>
+                    <tr>
+                        <th colspan="2" style="padding-left: 0; text-align: center;">Database Query</th>
+                    </tr>
+                    <tr>
+                        <th>Table Name:</th>
+                        <td>
+                            <!-- Combo box for list of tables found in the database. -->
+                            <?php
+                                // Declare a variable for the query.
+                                $query_show_tables = "SHOW TABLES";
+
+                                // Attempt to conenct to the DB, execute the query and get results.
+                                $result_show_tables = mysqli_query($connection, $query_show_tables);
+
+                                // Suppress the warning message epon loading the page first time.
+                                // Get the selected value from the combo box
+                                @$selected_table = $_GET['cbTableName'];
+
+                                // Display the combo box.
+                                echo '<select class="table-names-cb" name="cbTableName">';
+                                echo '<option value=""></option>';
+                                // Insert the each of the results into combo box.
+                                while ($table_name = $result_show_tables->fetch_assoc()) {
+                                    // Check if the current table name is the selected one
+                                    if ($table_name['Tables_in_' . $database] == $selected_table) {
+                                        echo '<option selected="selected" value="' . $table_name['Tables_in_' . $database] . '">' . $table_name['Tables_in_' . $database] . '</option>';
+                                    } else {
+                                        echo '<option value="' . $table_name['Tables_in_' . $database] . '">' . $table_name['Tables_in_' . $database] . '</option>';
+                                    }
+                                }
+                                echo '</select>';
+
+                                // Suppress the warning message.
+                                // Get the value from the combo box.
+                                // @$selected_table = $_GET['cbTableName'];
+                                // if (isset($_GET['cbTableName'])) {
+                                //     echo '<select class="table-names-cb" name="cbTableName">';
+                                //     while ($table_name = $result_show_tables->fetch_assoc()) {
+                                //         // Insert the each of the results into combo box.
+                                //         if ($table_name == $_GET['selected_table']) {
+                                //             echo '<option selected="selected" value="' . $table_name['Tables_in_' . $database] . '">' 
+                                //             . $table_name['Tables_in_' . $database] . 
+                                //             '</option>';
+                                //         } else {
+                                //         echo '<option value="' . $table_name['Tables_in_' . $database] . '">' 
+                                //             . $table_name['Tables_in_' . $database] . 
+                                //         '</option>';
+                                //         }
+                                //     }
+                                //     echo '</select>';
+                                // } else {
+                                //     // Insert the each of the results into combo box.
+                                //     echo '<select class="table-names-cb" name="cbTableName">';
+                                //     while ($table_name = $result_show_tables->fetch_assoc()) {
+                                //         echo '<option value="' . $table_name['Tables_in_' . $database] . '">' 
+                                //             . $table_name['Tables_in_' . $database] . 
+                                //         '</option>';
+                                //     }
+                                //     echo '</select>';
+                                // }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Action:</th>
+                        <td><input type="submit" value="Search from the database"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+
+        <div class="hidden-div"></div>
+
+        <div class="query-table">
+            <form action="index.php" method="post"></form>
+                <table>
+                    <tr>
+                        <th colspan="2" style="padding-left: 0; text-align: center;">Table Query</th>
+                    </tr>
+                    <tr>
+                        <th>ID:</th>
+                        <td><input type="text" name="txtID" id=""></td>
+                    </tr>
+                    <tr>
+                        <th>First Name:</th>
+                        <td><input type="text" name="txtFName" id=""></td>
+                    </tr>
+                    <tr>
+                        <th>Last Name:</th>
+                        <td><input type="text" name="txtLName" id=""></td>
+                    </tr>
+                    <tr>
+                        <th>Email Address:</th>
+                        <td><input type="email" name="txtEmail" id=""></td>
+                    </tr>
+                    <tr>
+                        <th>Gender:</th>
+                        <td>
+                            <div class="radio-choice">
+                                <input type="radio" id="male" name="rdoGender" value="Male">
+                                <label for="male">Male</label><br>
+                                <input type="radio" id="female" name="rdoGender" value="Female">
+                                <label for="female">Female</label><br>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Country:</th>
+                        <td><input type="text" name="txtCountry" id=""></td>
+                    </tr>
+                    <tr>
+                        <th>Action:</th>
+                        <td><input type="submit" value="Search from the database"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- TODO -->
+         <div id="manage-users-table">
+            <table class="manage-users-table" border=1>
+            <tr>
+                <th>Table Name</th>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email Address</th>
+                <th>Password</th>
+                <th>Gender</th>
+                <th>Country</th>
+                <th colspan="2">Actions</th>
+            </tr>
+
+            <?php
+
+
+            // Declare a variable for the query.
+            // $query = "SELECT * FROM $table ORDER BY id ASC";
+
+            // execute the query
+            // $result = mysqli_query($connection, $query);
+
+            // id, firstname, lastname, email, password, gender, country
+
+            // read/display the results
+            while($row = mysqli_fetch_assoc($result)) {
+                // mysqli_fetch_row(0)
+                // mysqli_fetch_array('firstname')
+                // mysqli_fetch_assoc('firstname')
+                // mysqli_fetch_assoc('0')
+            ?>
+
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['firstname']; ?></td>
+                <td><?php echo $row['lastname']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['password']; ?></td>
+                <td><?php echo $row['gender']; ?></td>
+                <td><?php echo $row['country']; ?></td>
+                <td><a href="editUser.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                <td><a href="">Delete</a></td>
+            </tr>
+            <?php
+            }
+
+            echo "</table>";
+
+            // close the connection
+            mysqli_close($connection);
+            ?>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+        <br><br><br><br><br>
+
+        <div id="footer-container-3-mobile">
+            <p class="black-text">Subscribe to our mailing list to be notified of latest news.</p><br>
+            <div class="subscription-form">
+                <form action="" method="post">
+                    <input type="email" name="email" placeholder="Enter your email address" class="subscribe-textbox" required><br><br>
+                    <input type="submit" value="Subscribe" class="subscribe-button">
+                </form>
+            </div>
+        </div>
+
+        <div class="hidden-footer-container-3-mobile"></div>
+
+        <br class="mobile-line-break">
+        <br class="mobile-line-break">
+        <br class="mobile-line-break">
+
+        <div id="footer-container" class="footer-text">
+            <div id="footer-container-2">
+                <p class="footer-text-2">Sitemap</p>
+                <ul>
+                    <a class="white-hyperlink" href="../index.php" class="white">
+                        <li class="padding-bottom">Home</li>
+                    </a>
+                    <a class="white-hyperlink" href="../quizzes/index.php" class="white">
+                        <li class="padding-bottom">Quizzes</li>
+                    </a>
+                    <a class="white-hyperlink" href="../tips/index.php" class="white">
+                        <li class="padding-bottom">Tips</li>
+                    </a>
+                    <a class="white-hyperlink" href="../donations/index.php" class="white">
+                        <li class="padding-bottom">Donations</li>
+                    </a>
+                    <a class="white-hyperlink" href="../contact/index.php" class="white">
+                        <li class="padding-bottom">Contact us</li>
+                    </a>
+                    <a class="white-hyperlink" href="../about/index.php" class="white">
+                        <li class="padding-bottom">About us</li>
+                    </a>
+                </ul>
+            </div>
+            <div id="footer-container-3">
+                <p class="black-text">Subscribe to our mailing list<br>to be notified of latest news.</p><br>
+                <div class="subscription-form">
+                    <form action="" method="post">
+                        <input type="email" name="email" placeholder="Enter your email address" class="subscribe-textbox" required><br><br>
+                        <input type="submit" value="Subscribe" class="subscribe-button">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
--- sql_query_database/ucdf2307_rwdd_group_4.sql
+++ sql_query_database/ucdf2307_rwdd_group_4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2024 at 12:41 PM
+-- Generation Time: Dec 25, 2024 at 02:40 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,19 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `contact_us_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email_address` varchar(200) NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `date_submitted` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `css_quiz_answers`
 --
 
 CREATE TABLE `css_quiz_answers` (
-  `id` int(11) NOT NULL,
-  `email` varchar(200) NOT NULL,
+  `css_quiz_answers_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
   `date_attempted` varchar(200) NOT NULL,
+  `date_submitted` varchar(200) NOT NULL,
   `q1_answer` varchar(200) NOT NULL,
   `q2_answer` varchar(200) NOT NULL,
   `q3_answer` varchar(200) NOT NULL,
   `q4_answer` varchar(200) NOT NULL,
   `q5_answer` varchar(200) NOT NULL,
   `q6_answer` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donations`
+--
+
+CREATE TABLE `donations` (
+  `donation_id` int(11) NOT NULL,
+  `email_address` varchar(200) NOT NULL,
+  `bank_account_number` int(20) NOT NULL,
+  `amount_donated` int(15) NOT NULL,
+  `date_submitted` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -46,9 +75,10 @@ CREATE TABLE `css_quiz_answers` (
 --
 
 CREATE TABLE `html_quiz_answers` (
-  `id` int(11) NOT NULL,
-  `email_address` varchar(200) NOT NULL,
+  `html_quiz_answers_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
   `date_attempted` varchar(200) NOT NULL,
+  `date_submitted` varchar(200) NOT NULL,
   `q1_answer` varchar(200) NOT NULL,
   `q2_answer` varchar(200) NOT NULL,
   `q3_answer` varchar(200) NOT NULL,
@@ -60,14 +90,28 @@ CREATE TABLE `html_quiz_answers` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mailing_list`
+--
+
+CREATE TABLE `mailing_list` (
+  `mailing_list_id` int(11) NOT NULL,
+  `email_address` varchar(200) NOT NULL,
+  `is_subscribed` tinyint(1) NOT NULL,
+  `date_first_subscribed` varchar(200) NOT NULL,
+  `date_modified` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
+  `email_address` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `gender` varchar(200) NOT NULL,
   `country` varchar(200) NOT NULL,
@@ -80,59 +124,121 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `gender`, `country`, `is_admin`, `date_created`, `date_modified`) VALUES
+INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email_address`, `password`, `gender`, `country`, `is_admin`, `date_created`, `date_modified`) VALUES
 (1, '', '', 'test@test.com', 'test', '', 'Malaysia', 1, '2024/12/23 04:11:09 am', ''),
-(37, '', '', 'testes@fesfes.com', 'fmesfioems', '', 'Malaysia', 0, '2024/12/23 04:56:51 am', ''),
-(38, '', '', 'test@test.com', 'testestestset', '', 'Malaysia', 0, '2024/12/23 04:57:30 am', ''),
-(39, '', '', '123@test.com', 'test', '', 'Malaysia', 0, '2024/12/24 11:13:01 pm', ''),
-(40, '', '', '123@test.com', 'utu35378957325', '', 'Malaysia', 0, '2024/12/24 11:14:28 pm', ''),
-(41, '', '', 'test123@gmail.com', '1234567890', '', 'Malaysia', 0, '2024/12/24 11:17:46 pm', ''),
-(80, '', '', 'test@test.com', 'test3213214214', '', 'Malaysia', 0, '2024/12/24 11:36:44 pm', ''),
-(81, '', '', 'test@test.com', 'test3213214214', '', 'Malaysia', 0, '2024/12/24 11:37:23 pm', '');
+(37, '', '', 'test2@test.com', 'fmesfioems', '', 'Malaysia', 0, '2024/12/23 04:56:51 am', ''),
+(38, '', '', 'test3@test.com', 'testestestset', '', 'Malaysia', 0, '2024/12/23 04:57:30 am', ''),
+(39, '', '', 'test4@test.com', 'test', '', 'Malaysia', 0, '2024/12/24 11:13:01 pm', ''),
+(40, '', '', 'test5@test.com', 'utu35378957325', '', 'Malaysia', 0, '2024/12/24 11:14:28 pm', ''),
+(41, '', '', 'test6@gmail.com', '1234567890', '', 'Malaysia', 0, '2024/12/24 11:17:46 pm', ''),
+(80, '', '', 'test7@test.com', 'test3213214214', '', 'Malaysia', 0, '2024/12/24 11:36:44 pm', ''),
+(81, '', '', 'test8@test.com', 'test3213214214', '', 'Malaysia', 0, '2024/12/24 11:37:23 pm', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`contact_us_id`);
+
+--
 -- Indexes for table `css_quiz_answers`
 --
 ALTER TABLE `css_quiz_answers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`css_quiz_answers_id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Indexes for table `donations`
+--
+ALTER TABLE `donations`
+  ADD PRIMARY KEY (`donation_id`);
 
 --
 -- Indexes for table `html_quiz_answers`
 --
 ALTER TABLE `html_quiz_answers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`html_quiz_answers_id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Indexes for table `mailing_list`
+--
+ALTER TABLE `mailing_list`
+  ADD PRIMARY KEY (`mailing_list_id`),
+  ADD KEY `email_address` (`email_address`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email_address` (`email_address`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `contact_us_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `css_quiz_answers`
 --
 ALTER TABLE `css_quiz_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `css_quiz_answers_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donations`
+--
+ALTER TABLE `donations`
+  MODIFY `donation_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `html_quiz_answers`
 --
 ALTER TABLE `html_quiz_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `html_quiz_answers_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mailing_list`
+--
+ALTER TABLE `mailing_list`
+  MODIFY `mailing_list_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `css_quiz_answers`
+--
+ALTER TABLE `css_quiz_answers`
+  ADD CONSTRAINT `css_quiz_answers_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `html_quiz_answers`
+--
+ALTER TABLE `html_quiz_answers`
+  ADD CONSTRAINT `html_quiz_answers_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `mailing_list`
+--
+ALTER TABLE `mailing_list`
+  ADD CONSTRAINT `mailing_list_ibfk_1` FOREIGN KEY (`email_address`) REFERENCES `users` (`email_address`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
